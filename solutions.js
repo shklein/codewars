6121c4de6ceb6a9e8cacc7bb38dfa2436caa9d12
Samantha Klein <saradurga@gmail.com>
2016-09-14 13:07:09 -0500

    charcodes not passing

--- solutions.js
+++ solutions.js
@@ -1,3 +1,4 @@
+//sorting
 function twoSort(s) {
   s.sort();
   var string = s[0];
@@ -9,8 +10,33 @@ function twoSort(s) {
 return array.join('***');
 }
 
+//greeting
 function greet(name){
   if(name === "Johnny")
     return "Hello, my love!";
   return "Hello, " + name + "!";
 }
+
+//charcodes
+var b = [];
+function isVow(a){
+  a.forEach(change);
+  console.log(b);
+}
+
+function change(c) {
+  var e = c;
+  var d = String.fromCharCode(c);
+  if (typeof e === 'string') {
+    b.push(e);
+  } else if (c.charCodeAt != NaN && d.match(/[aeiou]/)) {
+    b.push(String.fromCharCode(c));
+  } else {
+    b.push(c);
+  }
+ }
+
+
+var testArray = [118,117,120,121,117,98,'c',97,120,106,104,116,113,114,113,120,106];
+
+isVow(testArray);
